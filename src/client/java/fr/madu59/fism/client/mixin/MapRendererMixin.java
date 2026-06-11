24468fc88ed121ec95105ee29ce9429f48e3b5b6
maDU59_ <matthias.ducourant@gmail.com>
2026-06-11 12:57:18 +0200

    Simplified MapRendererMixin

--- src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
@@ -5,17 +5,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
 import fr.madu59.fism.client.compat.ModCompat;
 import net.minecraft.client.renderer.MapRenderer;
-import net.minecraft.client.renderer.SubmitNodeCollector;
-import net.minecraft.client.renderer.state.MapRenderState;
 
 @Mixin(MapRenderer.class)
 public abstract class MapRendererMixin {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    public <S> void fism$cancelMapRendering(final MapRenderState mapRenderState, final PoseStack poseStack, final SubmitNodeCollector submitNodeCollector, final boolean showOnlyFrame, final int lightCoords, CallbackInfo ci) {
+    public <S> void fism$cancelMapRendering(CallbackInfo ci) {
         if(ModCompat.isShadowPass()) {
             ci.cancel();
         }
