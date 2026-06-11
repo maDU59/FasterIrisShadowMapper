ba54ae8c4382213224ab2660cb9aca9b2bb6831e
maDU59_ <matthias.ducourant@gmail.com>
2026-06-11 20:51:44 +0200

    BackPorted to 1.21.3

--- src/client/java/fr/madu59/fism/client/mixin/AbstractSignRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/AbstractSignRendererMixin.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import fr.madu59.fism.client.compat.ModCompat;
-import net.minecraft.client.renderer.blockentity.AbstractSignRenderer;
+import net.minecraft.client.renderer.blockentity.SignRenderer;
 
-@Mixin(AbstractSignRenderer.class)
+@Mixin(SignRenderer.class)
 public abstract class AbstractSignRendererMixin {
     @Inject(method = "renderSignText", at = @At("HEAD"), cancellable = true)
     public void fism$cancelSignTextRendering(CallbackInfo ci){
