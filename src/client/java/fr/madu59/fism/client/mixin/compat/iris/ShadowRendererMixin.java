f428d9565f38a3ec2887573050d87c68def9986a
maDU59_ <matthias.ducourant@gmail.com>
2026-06-11 20:31:08 +0200

    Fixed backport

--- src/client/java/fr/madu59/fism/client/mixin/compat/iris/ShadowRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/compat/iris/ShadowRendererMixin.java
@@ -1,6 +1,5 @@
 package fr.madu59.fism.client.mixin.compat.iris;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,21 +7,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import fr.madu59.fism.client.FasterIrisShadowMapperClient;
 import fr.madu59.fism.client.compat.ModCompat;
-import net.irisshaders.iris.mixin.LevelRendererAccessor;
 import net.irisshaders.iris.shadows.ShadowRenderer;
-import net.minecraft.client.Camera;
-import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.renderer.MultiBufferSource.BufferSource;
-import net.minecraft.client.renderer.culling.Frustum;
-import net.minecraft.world.TickRateManager;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 @Mixin(ShadowRenderer.class)
 public class ShadowRendererMixin {
@@ -36,23 +30,14 @@ public class ShadowRendererMixin {
         FasterIrisShadowMapperClient.counter = 0;
     }
 
-    @Inject(
-        method = "extractVisibleBlockEntities",
-        at = @At("RETURN"
-        )
+    @Redirect(
+        method = "renderBlockEntities",
+        at = @At(value = "INVOKE", target = "render")
     )
-    private void fism$filterShadowBlockEntities(
-        LevelRendererAccessor accessor, BufferSource bufferSource, PoseStack modelView, float tickDelta, Camera camera, LevelRenderState levelRenderState, boolean lightsOnly, CallbackInfo ci
-    ) {
+    private void fism$filterShadowBlockEntities(BlockEntityRenderDispatcher dispatcher, BlockEntity be, float tickDelta, PoseStack modelView, MultiBufferSource bufferSource) {
         if (ModCompat.isShadowPass()) {
-            Iterator<BlockEntityRenderState> state = levelRenderState.blockEntityRenderStates.iterator();
-
-            while(state.hasNext()) {
-                BlockEntityRenderState blockEntityRenderState = (BlockEntityRenderState)state.next();
-                if (ModCompat.isOcclusionCulled(blockEntityRenderState.blockPos, blockEntityRenderState.blockEntityType)) {
-                    FasterIrisShadowMapperClient.counter += 1;
-                    state.remove();
-                }
+            if (!ModCompat.isOcclusionCulled(be.getBlockPos(), be.getType())) {
+                dispatcher.render(be, tickDelta, modelView, bufferSource);
             }
         }
     }
@@ -62,13 +47,15 @@ public class ShadowRendererMixin {
         at = @At(
             value = "INVOKE", 
             target = "add"
-        ),
+        )
     )
-    private void fism$filterShadowEntities(List<Entity> renderedEntities, Entity entity) {
+    private boolean fism$filterShadowEntities(List<Entity> renderedEntities, Object object) {
         if (ModCompat.isShadowPass()) {
+            Entity entity = (Entity) object;
             if (!ModCompat.isOcclusionCulled(entity.getBoundingBox())) {
                 renderedEntities.add(entity);
             }
         }
+        return true;
     }
 }
