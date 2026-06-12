e6fe7ed1ebd97364382c99c995b43ae0ac6d2b62
maDU59_ <matthias.ducourant@gmail.com>
2026-06-12 14:28:03 +0200

    Backported to 1.20.1

--- src/client/java/fr/madu59/fism/client/mixin/BannerRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/BannerRendererMixin.java
@@ -1,26 +1,31 @@
 package fr.madu59.fism.client.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.datafixers.util.Pair;
 
 import fr.madu59.fism.client.compat.ModCompat;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BannerRenderer;
 import net.minecraft.client.resources.model.Material;
+import net.minecraft.core.Holder;
 import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.level.block.entity.BannerPatternLayers;
+import net.minecraft.world.level.block.entity.BannerPattern;
 
 @Mixin(BannerRenderer.class)
 public abstract class BannerRendererMixin {
-    @Inject(method = "renderPatterns(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/minecraft/client/model/geom/ModelPart;Lnet/minecraft/client/resources/model/Material;ZLnet/minecraft/world/item/DyeColor;Lnet/minecraft/world/level/block/entity/BannerPatternLayers;Z)V", at = @At("HEAD"), cancellable = true)
-    private static <S> void fism$cancelRenderPatterns(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, ModelPart modelPart, Material material, boolean bl, DyeColor dyeColor, BannerPatternLayers bannerPatternLayers, boolean bl2, CallbackInfo ci) {
+    @Inject(method = "renderPatterns(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/minecraft/client/model/geom/ModelPart;Lnet/minecraft/client/resources/model/Material;ZLjava/util/List;Z)V", at = @At("HEAD"), cancellable = true)
+    private static <S> void fism$cancelRenderPatterns(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, ModelPart modelPart, Material material, boolean bl, List<Pair<Holder<BannerPattern>, DyeColor>> list, boolean bl2, CallbackInfo ci) {
         if(ModCompat.isShadowPass()) {
-            BannerRendererAccessor.fism$renderPatternLayerInvoke(poseStack, multiBufferSource, i, j, modelPart, material, dyeColor);
+            modelPart.render(poseStack, material.buffer(multiBufferSource, RenderType::entitySolid, bl2), i, j);
             ci.cancel();
         }
     }
