6a5cd7c728bbb83aa1e78df0aa5f13aceda246d5
maDU59_ <matthias.ducourant@gmail.com>
2026-06-15 16:22:54 +0200

    Reverted last change

--- src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.client.renderer.entity.ItemFrameRenderer;
 
 @Mixin(ItemFrameRenderer.class)
 public abstract class MapRendererMixin {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/MapItem;getSavedData(Lnet/minecraft/world/level/saveddata/maps/MapId;Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;"), cancellable = true)
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/MapItem;getSavedData(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;"), cancellable = true)
     public <S> void fism$cancelMapRendering(CallbackInfo ci) {
         if(ModCompat.isShadowPass()) {
             ci.cancel();
