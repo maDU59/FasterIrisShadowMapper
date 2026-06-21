aae0c50ddcb850d3efcbb1c00cd6b6ce3378cf24
maDU59_ <matthias.ducourant@gmail.com>
2026-06-21 13:43:20 +0200

    Fixed mixin target

--- src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
+++ src/client/java/fr/madu59/fism/client/mixin/MapRendererMixin.java
@@ -10,8 +10,15 @@ import net.minecraft.client.renderer.entity.ItemFrameRenderer;
 
 @Mixin(ItemFrameRenderer.class)
 public abstract class MapRendererMixin {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/MapItem;getSavedData(Ljava/lang/Integer;Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;"), cancellable = true)
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/MapItem;getSavedData(Ljava/lang/Integer;Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;"), cancellable = true, require = 0)
     public <S> void fism$cancelMapRendering(CallbackInfo ci) {
+        if(ModCompat.isShadowPass()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/MapItem;getSavedData(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;"), cancellable = true, require = 0)
+    public <S> void fism$cancelMapRenderingBis(CallbackInfo ci) {
         if(ModCompat.isShadowPass()) {
             ci.cancel();
         }
